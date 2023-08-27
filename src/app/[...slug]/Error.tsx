6837d1610117be3f27cd4190c4d585b141c079ad
Jason Taylor <jasonleetaylor88@gmail.com>
2023-08-27 18:07:40 +0100

    feat(error handling): include an error handler in the slug directory

---
+++ src/app/[...slug]/Error.tsx
@@ -0,0 +1,35 @@
+"use client"; // Error components must be Client Components
+
+import { Box, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useEffect } from "react";
+
+export default function Error({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
+
+	return (
+		<Box>
+			<Typography variant='h2' color={"warning.main"}>
+				Something went wrong! Given more time this is where I would implement a
+				proper error boundary handling system.
+			</Typography>
+			<Button
+				onClick={
+					// Attempt to recover by trying to re-render the segment
+					() => reset()
+				}
+			>
+				Try again
+			</Button>
+		</Box>
+	);
+}
