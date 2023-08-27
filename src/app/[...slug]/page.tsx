e56732fea139933be40a8d55297ff8961eb7f17f
Jason Taylor <jasonleetaylor88@gmail.com>
2023-08-27 13:15:08 +0100

    feat(filter accordion): create the outline of a facet filter accordion component

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,12 +1,35 @@
 // Renders the content for the products pages
 
-import { Grid } from "@mui/material";
+import FilterAccordion from "@/clientComponents/filterAccordion";
+import { Grid, Typography } from "@mui/material";
 
 export default function Products() {
 	return (
 		<Grid container spacing={2}>
 			<Grid item xs={12} sm={6} md={4} lg={3}>
-				Filtering
+				<Typography
+					variant='h2'
+					fontSize={"1.5rem"}
+					color='text.primary'
+					fontWeight={700}
+					marginBottom={"1rem"}
+				>
+					Filter By
+				</Typography>
+				<FilterAccordion
+					facetContents={[
+						{
+							key: "0",
+							element: <div>Test</div>,
+						},
+						{
+							key: "1",
+							element: <div>Test 2</div>,
+						},
+					]}
+					facetTitle={"Price"}
+					numInitialFacets={1}
+				/>
 			</Grid>
 			<Grid item xs={12} sm={6} md={8} lg={9}>
 				<Grid container spacing={2}>
---
+++ src/clientComponents/filterAccordion.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import {
+	Box,
+	Typography,
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+	Button,
+} from "@mui/material";
+import { FunctionComponent, ReactElement, useMemo, useState } from "react";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+
+interface IFlterAccordionProps {
+	facetContents: { key: string; element: ReactElement }[];
+	facetTitle: string;
+	numInitialFacets?: number;
+}
+
+const FilterAccordion: FunctionComponent<IFlterAccordionProps> = ({
+	facetContents,
+	facetTitle,
+	numInitialFacets,
+}) => {
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const toggleIsExpanded = () => setIsExpanded(prev => !prev);
+
+	const [isExtraExpanded, setIsExtraExpanded] = useState(false);
+
+	const toggleIsExtraExpanded = () => setIsExtraExpanded(prev => !prev);
+
+	const { extraFacets, initialFacets } = useMemo(() => {
+		const validNumInitialFacets = Boolean(numInitialFacets);
+
+		return {
+			initialFacets: validNumInitialFacets
+				? facetContents.slice(0, numInitialFacets)
+				: facetContents,
+			extraFacets: validNumInitialFacets
+				? facetContents.slice(numInitialFacets)
+				: undefined,
+		};
+	}, [facetContents, numInitialFacets]);
+
+	return (
+		<>
+			<Accordion
+				expanded={isExpanded}
+				onChange={toggleIsExpanded}
+				disableGutters
+			>
+				<AccordionSummary
+					expandIcon={isExpanded ? <RemoveIcon /> : <AddIcon />}
+					aria-controls='facet-price-content'
+					id='facet-price-header'
+					sx={{ borderBottom: "1px solid", borderColor: "grey.400" }}
+				>
+					<Typography variant='h3' fontSize='1.4rem'>
+						{facetTitle}
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails sx={{ padding: 0 }}>
+					{initialFacets.map(({ key, element }) => (
+						<Box key={key} sx={{ padding: "1rem" }}>
+							{element}
+						</Box>
+					))}
+				</AccordionDetails>
+			</Accordion>
+			{/* This is the extra facets */}
+			{extraFacets && (
+				<>
+					<Accordion expanded={isExtraExpanded} disableGutters>
+						<AccordionDetails sx={{ padding: 0 }}></AccordionDetails>
+						<AccordionSummary
+							aria-controls='facet-price-extra-content'
+							id='facet-price-extra-header'
+							sx={{
+								padding: 0,
+								minHeight: 0,
+								height: "fit-content",
+								margin: "-12px 0",
+							}}
+						>
+							<Box>
+								{extraFacets.map(({ key, element }) => (
+									<Box key={key} sx={{ padding: "1rem" }}>
+										{element}
+									</Box>
+								))}
+							</Box>
+						</AccordionSummary>
+					</Accordion>
+					<Box
+						flex={1}
+						display='flex'
+						justifyContent={"center"}
+						sx={{ backgroundColor: "grey.400", color: "text.primary" }}
+					>
+						<Button
+							variant='text'
+							onClick={toggleIsExtraExpanded}
+							sx={{
+								paddingY: "1rem",
+								color: "text.primary",
+								textDecoration: "underline",
+								textTransform: "none",
+							}}
+						>
+							{isExtraExpanded ? "Show Less" : "Show More"}
+						</Button>
+					</Box>
+				</>
+			)}
+		</>
+	);
+};
+
+export default FilterAccordion;
